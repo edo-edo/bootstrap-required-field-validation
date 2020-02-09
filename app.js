9a214d7a69db0d6eb80b99101b34d22a96950888
giorgi <51026202+edo-edo@users.noreply.github.com>
2020-02-09 19:38:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+class validForm {
+    validInput(input, sib) {
+        input.className = "form-control is-valid";
+        sib.className = 'valid-feedback';
+        sib.innerHTML = 'Looks good!';
+    }
+    invalidInput(input, sib) {
+        input.className = "form-control is-invalid";
+        sib.className = 'invalid-feedback';
+        sib.innerHTML = 'please fill out this field!';
+    }
+    showInfo() {
+        let parent = document.querySelector('#info');
+        let info = document.createElement('div')
+        info.id = 'child';
+        info.className = 'alert alert-success';
+        info.innerHTML = '<strong>Success!</strong> information Sent successfully'
+        parent.appendChild(info);
+        setTimeout(() => document.querySelector('#child').remove(), 3000);
+    }
+    deleteWarning(input, sib) {
+        input.value = '';
+        input.className = "form-control";
+        sib.removeAttribute("class");
+        sib.innerHTML = ''
+    }
+
+}
+
+
+const valid = new validForm();
+const btn = document.getElementById('btn');
+btn.addEventListener('click', (e) => {
+    let c = 0;
+    e.preventDefault()
+    const input = document.querySelectorAll('input');
+    input.forEach((field) => {
+        if (field.value !== '') {
+            c++;
+            valid.validInput(field, field.nextElementSibling);
+        } else {
+
+            valid.invalidInput(field, field.nextElementSibling);
+        }
+
+    })
+    if (c === 5) {
+        valid.showInfo();
+        input.forEach((field) => {
+            valid.deleteWarning(field, field.nextElementSibling);
+        })
+    }
+
+})
